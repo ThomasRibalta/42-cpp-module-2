6cb28d6e122710dbcba450ac1460d07b980e8ed1
Thomas <thomasribalta@gmail.com>
2024-10-28 23:54:00 +0100

    edit

--- cpp06/ex02/src/Base.cpp
+++ cpp06/ex02/src/Base.cpp
@@ -1,47 +1,70 @@
 #include "header/Base.hpp"
 
-Base::~Base(void) {
+Base::~Base(void)
+{
   return;
 }
 
-Base *generate(void) {
+Base *generate(void)
+{
   srand(time(NULL));
   int random = rand() % 3;
-  if (random == 0) {
+  if (random == 0)
+  {
     return new A();
-  } else if (random == 1) {
+  }
+  else if (random == 1)
+  {
     return new B();
-  } else {
+  }
+  else
+  {
     return new C();
   }
 }
 
-void identify(Base *p) {
-  if (dynamic_cast<A *>(p)) {
+void identify(Base *p)
+{
+  if (dynamic_cast<A *>(p))
+  {
     std::cout << "A" << std::endl;
-  } else if (dynamic_cast<B *>(p)) {
+  }
+  else if (dynamic_cast<B *>(p))
+  {
     std::cout << "B" << std::endl;
-  } else if (dynamic_cast<C *>(p)) {
+  }
+  else if (dynamic_cast<C *>(p))
+  {
     std::cout << "C" << std::endl;
   }
 }
 
-void identify(Base &p) {
-  try {
+void identify(Base &p)
+{
+  try
+  {
     A &a = dynamic_cast<A &>(p);
     (void)a;
     std::cout << "A" << std::endl;
-  } catch (std::bad_cast &bc) {
-    try {
+  }
+  catch (std::bad_cast &bc)
+  {
+    try
+    {
       B &b = dynamic_cast<B &>(p);
       (void)b;
       std::cout << "B" << std::endl;
-    } catch (std::bad_cast &bc) {
-      try {
+    }
+    catch (std::bad_cast &bc)
+    {
+      try
+      {
         C &c = dynamic_cast<C &>(p);
         (void)c;
         std::cout << "C" << std::endl;
-      } catch (std::bad_cast &bc) {
+      }
+      catch (std::bad_cast &bc)
+      {
         std::cout << "Unknown" << std::endl;
       }
     }
--- cpp09/ex00/src/BitcoinExchange.cpp
+++ cpp09/ex00/src/BitcoinExchange.cpp
@@ -3,7 +3,7 @@
 
 BitcoinExchange::BitcoinExchange(void)
 {
-  std::ifstream file; 
+  std::ifstream file;
 
   file.open("src/data.csv");
   if (file)
@@ -20,69 +20,70 @@ BitcoinExchange::BitcoinExchange(void)
     std::cout << "Erreur ouverture du fichier: " << "src/data.csv" << std::endl;
 }
 
-BitcoinExchange::~BitcoinExchange(){}
+BitcoinExchange::~BitcoinExchange() {}
 
 float BitcoinExchange::get_value_prev(std::string date)
 {
-    std::map<std::string, float>::iterator it;
-    std::string closest_date;
-    float closest_value = -1.0f;
+  std::map<std::string, float>::iterator it;
+  std::string closest_date;
+  float closest_value = -1.0f;
 
-    for (it = _data.begin(); it != _data.end(); ++it)
+  for (it = _data.begin(); it != _data.end(); ++it)
+  {
+    const std::string &current_date = it->first;
+    float current_value = it->second;
+
+    if (current_date <= date)
     {
-        const std::string& current_date = it->first;
-        float current_value = it->second;
-
-        if (current_date <= date)
-        {
-            if (closest_date.empty() || current_date >= closest_date)
-            {
-                closest_date = current_date;
-                closest_value = current_value;
-            }
-        }
+      if (closest_date.empty() || current_date >= closest_date)
+      {
+        closest_date = current_date;
+        closest_value = current_value;
+      }
     }
-    return closest_value;
+  }
+  return closest_value;
 }
 
-void BitcoinExchange::compare_data(std::string filename) {
-    std::ifstream file(filename);
+void BitcoinExchange::compare_data(std::string filename)
+{
+  std::ifstream file(filename);
 
-    if (file.is_open())
+  if (file.is_open())
+  {
+    std::string line;
+    getline(file, line);
+    while (getline(file, line))
     {
-        std::string line;
-        getline(file, line);
-        while (getline(file, line))
+      size_t separator_pos = line.find(" | ");
+      if (separator_pos == std::string::npos)
+        std::cout << "Error: bad input => " << line << std::endl;
+      else
+      {
+        std::string date = line.substr(0, separator_pos);
+        std::string number = line.substr(separator_pos + 3);
+        if (date.empty() || number.empty())
+          std::cout << "Error: bad input => " << line << std::endl;
+        else
         {
-            size_t separator_pos = line.find(" | ");
-            if (separator_pos == std::string::npos)
-              std::cout << "Error: bad input => " << line << std::endl;
-            else
-            {
-              std::string date = line.substr(0, separator_pos);
-              std::string number = line.substr(separator_pos + 3);
-              if (date.empty() || number.empty())
-                std::cout << "Error: bad input => " << line << std::endl;
-              else
-              {
-                float max = atof(number.c_str());
-                if (max >= INT_MAX && line[line.length() - 1] == '8')
-                  std::cout << "Error: too large a number." << std::endl;
-                else if (max < 0)
-                  std::cout << "Error: not a positive number." << std::endl;
-                else if (get_value_prev(date) < 0)
-                  std::cout << "Error: not previous date => " << line << std::endl;
-                else
-                {
-                  std::cout << date << " => " << number << " = " << (get_value_prev(date) * max) << std::endl;
-                }
-              }
-            }
-                }
-        file.close();
+          float max = atof(number.c_str());
+          if (max >= INT_MAX && line[line.length() - 1] == '8')
+            std::cout << "Error: too large a number." << std::endl;
+          else if (max < 0)
+            std::cout << "Error: not a positive number." << std::endl;
+          else if (get_value_prev(date) < 0)
+            std::cout << "Error: not previous date => " << line << std::endl;
+          else
+          {
+            std::cout << date << " => " << number << " = " << (get_value_prev(date) * max) << std::endl;
+          }
+        }
+      }
     }
-    else
-    {
-        std::cout << "Erreur ouverture du fichier: " << filename << std::endl;
-    }
+    file.close();
+  }
+  else
+  {
+    std::cout << "Erreur ouverture du fichier: " << filename << std::endl;
+  }
 }
